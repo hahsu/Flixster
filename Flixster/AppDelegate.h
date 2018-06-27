32fb849a05c50d919c5d5041e04b3af87dd2aa02
Hannah Hsu <hahsu@fb.com>
2018-06-27 11:07:50 -0700

    Initial Commit

---
+++ Flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flixster
+//
+//  Created by Hannah Hsu on 6/27/18.
+//  Copyright © 2018 Hannah Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
